663a50e98a4308d1336c7dd23c651885799eb20e
vinayak <vdy1141@gmail.com>
2025-01-15 18:35:56 +0530

    Added calculator functionalities in the application

---
+++ functionalities.py
@@ -0,0 +1,13 @@
+def addition(num1, num2):
+    return num1 + num2
+
+def subtraction(num1, num2):
+    return num1 - num2
+
+def multiplication(num1, num2):
+    return num1 * num2
+
+def division(num1, num2):
+    if not num2:
+        return "Can't divide by zero"
+    return num1 / num2
--- main.py
+++ main.py
@@ -1,1 +1,7 @@
+from functionalities import addition, subtraction, multiplication, division
 print("Hello World")
+
+print(addition(20,10))
+print(subtraction(20,10))
+print(multiplication(20,10))
+print(division(20,10))
